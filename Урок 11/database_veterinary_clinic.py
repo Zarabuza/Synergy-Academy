41ddad18fe565d6817566337711e98f3bf24ad62
Zarabuza <nickbugakov@yandex.ru>
2024-05-03 23:29:04 +0300

    Урок 11 обновление

--- Урок 11/database_veterinary_clinic.py
+++ Урок 11/database_veterinary_clinic.py
@@ -1,4 +1,3 @@
-from collections import deque
 
 pets = dict()
 pets_tmp = dict()
@@ -24,22 +23,11 @@ def create():
     
     return pets
 
-
-def read(num):
-    animal_name = list(pets[num])[num - 1]
-    print(f'Это {pets[num][animal_name]["animal_specials"]} по кличке "{animal_name}". Возраст питомца: {pets[num][animal_name]["animal_age"]} {get_suffix(pets[num][animal_name]['animal_age'], num)}. Имя владельца: {pets[num][animal_name]["owners_name"]}')
-
-def update():
-    
-def delete(num):
-    key = num
-    if key in pets:
-        del pets[key]
-        print(f"Элемент с идентификатором {key} удален")
-    else:
-        print("Элемент не найден")
-    
 def get_pet(ID):
+    if ID in pets:
+        return True
+    else:
+        return False
 
 def get_suffix(age, num):
     animal_name = list(pets[num])[num - 1]
@@ -54,18 +42,60 @@ def get_suffix(age, num):
     
     return years_type
 
-def pets_list():
+def read(num):
+    if get_pet(num):
+        animal_name = list(pets[num])[num - 1]
+        print(f'Это {str(pets[num][animal_name]["animal_specials"]).lower()} по кличке "{animal_name}". Возраст питомца: {pets[num][animal_name]["animal_age"]} {get_suffix(pets[num][animal_name]['animal_age'], num)}. Имя владельца: {pets[num][animal_name]["owners_name"]}')
+    else:
+        read(num = int(input('Питомца с таким идентификатором не существует, попробуйте ещё раз: ')))
 
+def update(num):
+    
+    if get_pet(num):
+        keys = ['animal_specials', 'animal_age', 'owners_name']
+
+        questions = ['Как зовут вашего питомца? ', 'Укажите вид вашего животного ', 'Введите возраст вашего питомца ', 'Ваше имя: ']
+
+        pets_variables = dict.fromkeys(keys)
+
+        pets_change = dict()
+
+        for i in range(4):
+            if i == 0:
+                animal_name = input(questions[i])
+                pets_change[animal_name] = 0
+            else:
+                pets_variables[keys[i-1]] = input(questions[i])
+
+        pets_change[animal_name] = pets_variables
+        pets[num] = pets_change
+    else:
+        update(num = int(input('Питомца с таким идентификатором не существует, попробуйте ещё раз: ')))
+
+def delete(num):
+    if get_pet(num):
+        key = num
+        if key in pets:
+            del pets[key]
+            print(f"Элемент с идентификатором {key} удален")
+        else:
+            print("Элемент не найден")
+    else:
+        delete(num = int(input('Питомца с таким идентификатором не существует, попробуйте ещё раз: ')))
+
+
+command = input('Пожалуйста, введите одну из комманд "create", "read", "update", "delete" или "stop": ')
 while command != 'stop':
-    command = input('Пожалуйста, введите одну из комманд "create", "read", "update", "delete" или "stop": ')
-    if command == create:
-        create(command)
-    elif command == read:
+    if command == 'create':
+         create()
+    elif command == 'read':
         read(int(input('Введите идентификатор питомца: ')))
-    elif command == update:
-        update(command)
-    elif command == delete:
+    elif command == 'update':
+        update(int(input('Введите идентификатор питомца, информацию о котором вы хотите обновить: ')))
+    elif command == 'delete':
         delete(int(input('Введите идентификатор питомца, информацию о котором вы хотите удалить: ')))
     else:
-        print('Пожалуйста, проверьте правильность введённой вами команды')
-        continue
+        print('Проверьте правильность введённой вами команды')
+        command = input('Пожалуйста, введите одну из комманд "create", "read", "update", "delete" или "stop": ')
+
+print(pets)
--- Урок 11/test.py
+++ Урок 11/test.py
@@ -23,8 +23,11 @@ def create():
     
     return pets
 
-
-create()
+def get_pet(ID):
+    if ID in pets:
+        return True
+    else:
+        return False
 
 def get_suffix(age, num):
     animal_name = list(pets[num])[num - 1]
@@ -39,49 +42,60 @@ def get_suffix(age, num):
     
     return years_type
 
-
 def read(num):
-    animal_name = list(pets[num])[num - 1]
-    print(f'Это {str(pets[num][animal_name]["animal_specials"]).lower()} по кличке "{animal_name}". Возраст питомца: {pets[num][animal_name]["animal_age"]} {get_suffix(pets[num][animal_name]['animal_age'], num)}. Имя владельца: {pets[num][animal_name]["owners_name"]}')
-
-read(num = int(input('Введите идентификатор питомца: ')))
+    if get_pet(num):
+        animal_name = list(pets[num])[num - 1]
+        print(f'Это {str(pets[num][animal_name]["animal_specials"]).lower()} по кличке "{animal_name}". Возраст питомца: {pets[num][animal_name]["animal_age"]} {get_suffix(pets[num][animal_name]['animal_age'], num)}. Имя владельца: {pets[num][animal_name]["owners_name"]}')
+    else:
+        read(num = int(input('Питомца с таким идентификатором не существует, попробуйте ещё раз: ')))
 
 def update(num):
     
-    old_name = list(pets[num])[num - 1]
+    if get_pet(num):
+        keys = ['animal_specials', 'animal_age', 'owners_name']
 
-    keys = ['animal_specials', 'animal_age', 'owners_name']
+        questions = ['Как зовут вашего питомца? ', 'Укажите вид вашего животного ', 'Введите возраст вашего питомца ', 'Ваше имя: ']
 
-    questions = ['Как зовут вашего питомца? ', 'Укажите вид вашего животного ', 'Введите возраст вашего питомца ', 'Ваше имя: ']
+        pets_variables = dict.fromkeys(keys)
 
-    pets_variables = dict.fromkeys(keys)
+        pets_change = dict()
 
-    for i in range(4):
-        if i == 0:
-            animal_name = input(questions[i])
-            pets_tmp[animal_name] = 0
-        else:
-            pets_variables[keys[i-1]] = input(questions[i])
-    
-    pets_tmp[old_name] = pets_variables
-    print(pets_tmp)
-    pets[num] = pets_tmp
-    print(pets)
-    
-update(int(input('Введите идентификатор питомца, информацию о котором вы хотите обновить: ')))
+        for i in range(4):
+            if i == 0:
+                animal_name = input(questions[i])
+                pets_change[animal_name] = 0
+            else:
+                pets_variables[keys[i-1]] = input(questions[i])
 
-print(pets)
-
-read(num = int(input('Введите идентификатор питомца: ')))
+        pets_change[animal_name] = pets_variables
+        pets[num] = pets_change
+    else:
+        update(num = int(input('Питомца с таким идентификатором не существует, попробуйте ещё раз: ')))
 
 def delete(num):
-    key = num
-    if key in pets:
-        del pets[key]
-        print(f"Элемент с идентификатором {key} удален")
+    if get_pet(num):
+        key = num
+        if key in pets:
+            del pets[key]
+            print(f"Элемент с идентификатором {key} удален")
+        else:
+            print("Элемент не найден")
     else:
-        print("Элемент не найден")
+        delete(num = int(input('Питомца с таким идентификатором не существует, попробуйте ещё раз: ')))
 
-delete(int(input('Введите идентификатор питомца, информацию о котором вы хотите удалить: ')))
+
+command = input('Пожалуйста, введите одну из комманд "create", "read", "update", "delete" или "stop": ')
+while command != 'stop':
+    if command == 'create':
+         create()
+    elif command == 'read':
+        read(int(input('Введите идентификатор питомца: ')))
+    elif command == 'update':
+        update(int(input('Введите идентификатор питомца, информацию о котором вы хотите обновить: ')))
+    elif command == 'delete':
+        delete(int(input('Введите идентификатор питомца, информацию о котором вы хотите удалить: ')))
+    else:
+        print('Проверьте правильность введённой вами команды')
+        command = input('Пожалуйста, введите одну из комманд "create", "read", "update", "delete" или "stop": ')
 
 print(pets)
