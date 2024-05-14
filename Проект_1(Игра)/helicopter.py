4183d19e31b70fa265f4d44fe6593e712cd39aa9
Zarabuza <nickbugakov@yandex.ru>
2024-05-14 20:17:04 +0300

    Игра

---
+++ Проект_1(Игра)/helicopter.py
@@ -0,0 +1,25 @@
+from utils import randcell
+
+class Helicopter:
+    def __init__(self, w, h):
+        rc = randcell(w, h)
+        rx, ry = rc[0], rc[1]
+        self.x = rx
+        self.y = ry
+        self.w = w
+        self.h = h
+        self.tank = 0
+        self.mxtank = 1
+        self.score = 0
+        self.lives = 2
+ 
+
+    def move(self, dx, dy):
+        nx, ny = dx + self.x, dy + self.y
+        if (nx >= 0 and ny >= 0 and nx < self.h and ny < self.w):
+            self.x, self.y = nx, ny 
+    
+    def print_stats(self):
+        print("💧", self.tank, '/', self.mxtank, sep = "", end = "  |  ")
+        print("🏆", self.score,  end = "  |  ")
+        print("❤️", self.lives)
